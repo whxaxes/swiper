4ef41ff9a71e34ba4edb3992884d086e46f45554
wanghx <whx89768@alibaba-inc.com>
2017-07-28 11:35:38 +0800

    feat: 支持局部滚动 & 优化回弹效果

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
  * Created by jfengjiang on 2015/1/14.
  */
 
-
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var header = require('gulp-header');
@@ -16,67 +15,78 @@ var tap = require('gulp-tap');
 var browserSync = require('browser-sync');
 var pkg = require('./package.json');
 
-gulp.task('build', function () {
-    gulp.src('src/example/**/*')
-        .pipe(gulp.dest('dist/example'));
+gulp.task('build', function() {
+  gulp.src('src/example/**/*').pipe(gulp.dest('dist/example'));
 
-    var banner = ['/*!',
-        ' * <%= pkg.name %> - <%= pkg.description %>',
-        ' * @version v<%= pkg.version %>',
-        ' * @link <%= pkg.repository.url %>',
-        ' * @license <%= pkg.license %>',
-        ' */',
-        ''].join('\n');
+  var banner = [
+    '/*!',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.repository.url %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    '',
+  ].join('\n');
 
-    gulp.src('src/swiper.less')
-        .pipe(less().on('error', function (e){
-            console.error(e.message);
-            this.emit('end');
-        }))
-        .pipe(autoprefixer())
-        .pipe(header(banner, { pkg : pkg } ))
-        .pipe(gulp.dest('dist'))
-        .pipe(minify())
-        .pipe(rename(function (file){
-            file.basename += '.min';
-        }))
-        .pipe(gulp.dest('dist'));
+  gulp
+    .src('src/swiper.less')
+    .pipe(
+      less().on('error', function(e) {
+        console.error(e.message);
+        this.emit('end');
+      })
+    )
+    .pipe(autoprefixer())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest('dist'))
+    .pipe(minify())
+    .pipe(
+      rename(function(file) {
+        file.basename += '.min';
+      })
+    )
+    .pipe(gulp.dest('dist'));
 
-    gulp.src('src/swiper.js')
-        .pipe(tap(function(file, t){
-            var contents = file.contents.toString();
-            contents = contents.replace('${version}', pkg.version);
-            file.contents = new Buffer(contents);
-        }))
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(header(banner, { pkg : pkg } ))
-        .pipe(gulp.dest('dist'))
-        .pipe(uglify())
-        .pipe(rename(function (file){
-            file.basename += '.min';
-        }))
-        .pipe(gulp.dest('dist'));
+  gulp
+    .src('src/swiper.js')
+    .pipe(
+      tap(function(file, t) {
+        var contents = file.contents.toString();
+        contents = contents.replace('${version}', pkg.version);
+        file.contents = new Buffer(contents);
+      })
+    )
+    // .pipe(jshint())
+    // .pipe(jshint.reporter('default'))
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+    .pipe(
+      rename(function(file) {
+        file.basename += '.min';
+      })
+    )
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('server', function () {
-    browserSync.init({
-        server: {
-            baseDir: "./dist"
-        },
-        ui: {
-            port: 8081,
-            weinre: {
-                port: 9090
-            }
-        },
-        port: 8080,
-        startPath: '/example'
-    });
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+    ui: {
+      port: 8081,
+      weinre: {
+        port: 9090,
+      },
+    },
+    port: 8080,
+    startPath: '/example',
+  });
 });
 
-gulp.task('watch', ['build', 'server'], function (){
-    gulp.watch('src/**/*', ['build']);
+gulp.task('watch', ['build', 'server'], function() {
+  gulp.watch('src/**/*', ['build']);
 });
 
 gulp.task('default', ['watch']);
